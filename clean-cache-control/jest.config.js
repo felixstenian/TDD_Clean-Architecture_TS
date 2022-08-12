3899a159d09d6c28da87f2fce8487ba6daf2c5b9
felixstenian <felixstenian@gmail.com>
2022-08-12 10:57:03 -0300

    add jest

---
+++ clean-cache-control/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testEnviroment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMaper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
