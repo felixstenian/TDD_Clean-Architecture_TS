1be98699f14df176497e3ffb95a9ed744166bc5a
felixstenian <felixstenian@gmail.com>
2022-08-22 19:53:16 -0300

    add LoadPurchases useCase

--- clean-cache-control/src/domain/usecases/save-purchases.ts
+++ clean-cache-control/src/domain/usecases/save-purchases.ts
@@ -1,12 +1,8 @@
+import { PurchasesModel } from '@/domain/models'
 export interface SavePurchases {
   save: (purchases: SavePurchases.Params[]) => Promise<void>
-  // save: (purchases: Array<SavePurchases.Params>) => Promise<void>
 }
 
 export namespace SavePurchases {
-  export type Params = {
-    id: string
-    date: Date
-    value: number
-  }
+  export type Params = PurchasesModel
 }
